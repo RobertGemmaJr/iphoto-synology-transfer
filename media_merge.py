f64280d311530605cc03acd746c4db28f8814892
Robert Gemma <38439940+RobertGemmaJr@users.noreply.github.com>
2023-01-19 18:53:32 -0500

    feat: Extract date and create folders

--- media_merge.py
+++ media_merge.py
@@ -1,121 +1,69 @@
-import re
-
-import datetime
-
-import exifread
 import os
 import shutil
-import mimetypes
+import datetime
 import ffmpeg
+from pathlib import Path
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-DIR = os.path.join(os.path.expanduser('~'), 'Desktop', 'Rob Photos (temp)')
-METADATA = ["Name", "Size", "Item type", "Date modified", "Date taken"]
-            
-# Loop over all photos
-# Extract datetime
-# Extract year, month
-# Create year, month folder (Inside "Rob Photos (temp)") if it doesn't exist
-# Copy file to that folder (Make sure to use shutil.copy2)
-# Loop over all videos, do the same
+DIR = Path(Path.expanduser(Path("~")), "Desktop", "Rob Photos (temp)")
 
-def isImage(file: str):
-    mimestart = mimetypes.guess_type(file)[0]
-    return mimestart.split('/')[0] == 'image'
+# Use PIL to transfer the photos by datetime
+def get_photos(fileDict: dict):
+    for file in [p for p in Path(DIR, "Photos").rglob("*") if p.is_file()]:
+        exif = {TAGS[k]: v for k, v in Image.open(file).getexif().items() if k in TAGS}
+        date = datetime.datetime.strptime(exif["DateTime"], "%Y:%m:%d %H:%M:%S").date()
+        
+        fileDict[file] = date
+    return fileDict
 
-def isVideo(file: str):
-    type = mimetypes.guess_type(file)[0]
-    return type.split('/')[0] == 'video'
 
-def get_datetime(file: str):
-    assert os.path.exists(file)
-    
-    if(isImage(file)):
-        # Using exifread
-        with open(file, 'rb') as f:
-            tags = exifread.process_file(f)
-            # print(tags.keys())
-        
-        
-        # Using ffmpeg
-        # t = ffmpeg.probe(file)
-        # streams = t['streams']
-        # format = t['format']
-        # print(streams[0].keys())
-        # print(format.keys())
-        
-        
-        
-        # Using PIL
-        # image = Image.open(file)
-        # exif = { TAGS[k]: v for k, v in image.getexif().items() if k in TAGS }
-        # print('Image: ', exif['DateTime'])
-        # return exif['DateTime']
-    elif(isVideo(file)):
-        print(file)
-        video = ffmpeg.probe(os.path.abspath(file))
-        print(video)
-        return 'video'
-    else:
-        print(f"{file} is not a media file, skipped")
-        return None
-    
+# Use ffmpeg to transfer the videos by datetime
+def get_videos(fileDict: dict):
+    for file in [p for p in Path(DIR, "Videos").rglob("*") if p.is_file()]:
+            probe = ffmpeg.probe(file)
+
+            # TODO: Some don't have creation_time
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\GHDL5952.mp4
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\IMG_1791.mp4
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\ONYQ6525.mp4
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\OUKJ5151.mp4
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\URI Day of Giving 2021.mp4
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\WACY1703.mp4
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\WQDX6014.mp4
+            # C:\Users\gemr1\Desktop\Rob Photos (temp)\Videos\XHQC6528.mp4
+            try:
+                dateT = probe["format"]["tags"]["creation_time"]
+                date = datetime.datetime.strptime(dateT, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+                fileDict[file] = date
+            except Exception:
+                print("FAILED:", file)
+    return fileDict
 
 
 def main():
-    
-    # Get directory
-    # dir = os.path.join(os.path.expanduser('~'), 'Desktop', 'Rob Photos (temp)')
-    assert os.path.exists(DIR)
-    
-    
-    # Get full paths to the files
-    # dir should contain 2 folders - one for photos and one for videos, will recurse either way
-    # TODO: Just use PIL? It seems easier?
-    fileDict = {}
-    for root, subdirs, files in os.walk(os.path.join(DIR, 'Photos')):
-        for file in files:
-            file = os.path.join(root, file)
-            with open(file, 'rb') as f:
-                tags = exifread.process_file(f)
-                fileDict[f.name] = tags['EXIF DateTimeOriginal']
-    print(fileDict)
-    
-    # TODO: Read metadata for video files
-    for root, subdirs, files in os.walk(os.path.join(DIR, 'Videos')):
-        for file in files:
-            file = os.path.join(root, file)
-            print(file)
-            with open(file, 'rb') as f:
-                tags = exifread.process_file(f)
-                print(tags)
-                fileDict[f.name] = tags['EXIF DateTimeOriginal']
-    print(fileDict)
-                
-                
-    # EXIF DateTimeOriginal, Image DateTimeOriginal
-            
-    
-    # Create output directory (forced)
-    # dest = os.path.join(DIR, 'out')
-    # if(os.path.exists(dest)):
-    #     shutil.rmtree(dest)
-    # os.mkdir(dest)
-    # assert os.path.exists(dest)
-    
-    # Sort path by datetime
-    # paths.sort(key=get_datetime)
-        
-    # exif = { TAGS[k]: v for k, v in imageData.items() if k in TAGS }
-    # print(exif)
-    # for tag, data in exif.items():
-    #     # Decode bytes 
-    #     if isinstance(data, bytes): data = data.decode()
-    #     print(f"{tag:15}: {data}")
-    
     # TODO: 1.jpeg is wrong, save correct DateTime
-        
+    assert Path.exists(DIR)
+
+    fileDict: dict[str, datetime.date] = {}
+    get_photos(fileDict)
+    get_videos(fileDict)
+
+    # Create output directory (forced)
+    dest = Path(DIR, "out")
+    if Path.exists(dest):
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+    assert Path.exists(dest)
+
+    for path, date in fileDict.items():
+        print(f"{path} {date.year} {date.month}")
+        year = str(date.year)
+        month = str(date.month)
+        # Create year/ if not created
+        # Create year/month/ if not created
+        # Path(dest, year).mkdir(parents=True, exist_ok=True)
+        Path(dest, year, month).mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == "__main__":
